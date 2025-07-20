9620f3c9e137f5442d12c55e667a8684e846b8f5
wangjq4214 <w1637894214@163.com>
2025-07-20 21:15:51 +0800

    ✨ fix: fix the error font name

--- config/fonts.lua
+++ config/fonts.lua
@@ -5,7 +5,7 @@ local font_size = platform.is_mac and 16 or 14
 
 return {
   font = wezterm.font_with_fallback({
-    'Maple Mono Normal NF CN',
+    'Maple Mono NF CN',
     'Fira Code',
     'JetBrains Mono',
     'CaskaydiaCove Nerd Font',
