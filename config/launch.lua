82ad97bf18b118b7b59fb9e1140c328ebe0eae65
wangjq4214 <w1637894214@163.com>
2026-04-21 17:50:42 +0800

    ✨ feat: 更新 Windows 默认程序为 Nushell，并添加到启动菜单

--- config/launch.lua
+++ config/launch.lua
@@ -6,9 +6,10 @@ local options = {
 }
 
 if platform.is_win then
-  options.default_prog = { 'powershell' }
+  options.default_prog = { 'nu' }
   options.launch_menu = {
     { label = 'PowerShell Core', args = { 'powershell' } },
+    { label = 'nushell', args = { 'nu' } },
     {
       label = 'UCRT64 MSYS2',
       args = { 'C:/msys64/msys2_shell.cmd', '-defterm', '-here', '-no-start', '-ucrt64' },
